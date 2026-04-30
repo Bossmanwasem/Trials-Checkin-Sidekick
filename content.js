6a0340bbe8f04d62cb079d7baa9ef99c54686718
Bossmanwasem <jordanwasem93@gmail.com>
2026-04-30 14:01:20 -0500

    Add Smartbox Blue overrides for ContentAdmin/pageContent/alphabet selector set

--- content.js
+++ content.js
@@ -1345,9 +1345,15 @@ function applyCrmThemeStyle(themeVars = {}, mode = "") {
 
     /* User-reported remaining light selectors */
     #ContentAdmin > div:nth-child(7),
+    #ContentAdmin,
+    #ContentAdmin > div:nth-child(7),
     #ContentAdmin > div:nth-child(10),
+    #divAlphabet,
+    #aspnetForm > div.pageContent,
+    #aspnetForm > div.pageContent > table > tbody > tr:nth-child(1) > td > table > tbody > tr > td:nth-child(1),
     #aspnetForm > div.pageContent > table > tbody > tr:nth-child(1) > td > table > tbody > tr > td:nth-child(2) > table > tbody > tr > td,
-    #aspnetForm > div.pageContent > table > tbody > tr:nth-child(2) > td:nth-child(1) {
+    #aspnetForm > div.pageContent > table > tbody > tr:nth-child(2) > td:nth-child(1),
+    #aspnetForm > div.pageContent > table > tbody > tr:nth-child(2) > td:nth-child(2) {
       background-color: var(--ttmt-surface) !important;
       color: var(--ttmt-text) !important;
       border-color: var(--ttmt-border) !important;
